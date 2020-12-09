f3900090b4bc6f2d3476891165ba2d55507f6aed
Joakim Carlsson <hej@joakimcarlsson.se>
2020-12-09 08:21:38 +0100

    Made the check infinite

--- DbWatcherMVC2/DbWatcherMVC2/Controllers/HomeController.cs
+++ DbWatcherMVC2/DbWatcherMVC2/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using DbWatcherMVC2.Models;
 using DbWatcherMVC2.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,17 +18,55 @@ namespace DbWatcherMVC2.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IRepository _repo;
 
-        public HomeController(ILogger<HomeController> logger, IRepository repo)
+        string cnnString = "";
+
+        public HomeController(ILogger<HomeController> logger, IRepository repo, IConfiguration config)
         {
             _logger = logger;
             _repo = repo;
+            cnnString = config.GetConnectionString("DefaultConnection");
         }
 
         public IActionResult Index()
         {
-            _repo.GetAllPersons();
+            //_repo.GetAllPersons();
+
+            Task.Run(() => {
+                DbChecker(); 
+            });
 
             return View();
+        }
+
+
+        private void DbChecker()
+        {
+            using (var cnn = new SqlConnection(cnnString))
+            {
+                cnn.Open();
+
+                SqlDependency.Start(cnnString);
+
+                var cmdText = "SELECT Id, FirstName, LastName FROM dbo.Persons";
+
+                var cmd = new SqlCommand(cmdText, cnn);
+
+                var dependency = new SqlDependency(cmd);
+
+                dependency.OnChange += new OnChangeEventHandler(dbChangeNotification);
+
+                var reader = cmd.ExecuteReader();
+            }
+        }
+
+        private void dbChangeNotification(object sender, SqlNotificationEventArgs e)
+        {
+            using (var writer = new StreamWriter(Directory.GetCurrentDirectory() + "test.txt", true))
+            {
+                writer.WriteLine($"{DateTime.UtcNow} - Something changed");
+
+                DbChecker();
+            }
         }
 
         public IActionResult Privacy()
